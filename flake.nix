5e37b37a68a489f1f64c020a35f994446a8ea09c
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-03-28 00:23:29 +0000

    feat: init

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        name = "man-dskit";
+        version = "0.1.0";
+        nodejs = pkgs.nodejs_21;
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [pkgs.prefetch-npm-deps];
+            inputsFrom = [self'.packages.default];
+          };
+        };
+
+        packages = {
+          default = pkgs.buildNpmPackage {
+            inherit version nodejs;
+            pname = name;
+            src = ./.;
+            npmDepsHash = "sha256-OK0AEVUn58LUygS2tqROBGdaW4gIRBiAA/ev+0PiLbg=";
+            installPhase = ''
+              cp --no-preserve=mode -r build $out
+            '';
+          };
+        };
+      };
+    };
+}
